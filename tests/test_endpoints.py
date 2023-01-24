6172dcf72ac92ec16b15898afb573cfd47b4888e
Liam DeVoe <orionldevoe@gmail.com>
2023-01-24 17:11:38 -0500

    alias more api instances

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -2,52 +2,57 @@ from datetime import datetime
 from unittest import TestCase
 
 from ossapi import (RankingType, BeatmapsetEventType, AccessDeniedError,
-    InsufficientScopeError, Mod, GameMode, NewsPostKey)
+    InsufficientScopeError, Mod, GameMode)
 
-from tests import (api_v2, api_v2_full, api_v2_dev, TestCaseAuthorizationCode,
-    TestCaseDevServer)
+from tests import (
+    TestCaseAuthorizationCode, TestCaseDevServer,
+    api_v2 as api,
+    api_v2_full as api_full,
+    api_v2_dev as api_dev
+)
+
 
 class TestBeatmapsetDiscussionPosts(TestCase):
     def test_deserialize(self):
-        api_v2.beatmapset_discussion_posts()
+        api.beatmapset_discussion_posts()
 
 class TestUserRecentActivity(TestCase):
     def test_deserialize(self):
-        api_v2.user_recent_activity(12092800)
+        api.user_recent_activity(12092800)
 
 class TestSpotlights(TestCase):
     def test_deserialize(self):
-        api_v2.spotlights()
+        api.spotlights()
 
 class TestUserBeatmaps(TestCase):
     def test_deserialize(self):
-        api_v2.user_beatmaps(user_id=12092800, type_="most_played")
+        api.user_beatmaps(user_id=12092800, type_="most_played")
 
 class TestUserKudosu(TestCase):
     def test_deserialize(self):
-        api_v2.user_kudosu(user_id=3178418)
+        api.user_kudosu(user_id=3178418)
 
 class TestBeatmapScores(TestCase):
     def test_deserialize(self):
-        api_v2.beatmap_scores(beatmap_id=1981090)
+        api.beatmap_scores(beatmap_id=1981090)
 
 class TestBeatmap(TestCase):
     def test_deserialize(self):
-        api_v2.beatmap(beatmap_id=221777)
+        api.beatmap(beatmap_id=221777)
 
         # beatmap with a diff owner
-        bm = api_v2.beatmap(beatmap_id=1604098)
+        bm = api.beatmap(beatmap_id=1604098)
         # might need to be updated when
         # https://github.com/ppy/osu-web/issues/9784 is addressed.
         self.assertIsNone(bm.owner)
 
 class TestBeatmapset(TestCase):
     def test_deserialize(self):
-        api_v2.beatmapset(beatmap_id=3207950)
+        api.beatmapset(beatmap_id=3207950)
 
 class TestBeatmapsetEvents(TestCase):
     def test_deserialize(self):
-        api_v2.beatmapset_events()
+        api.beatmapset_events()
 
     def test_all_types(self):
         # beatmapset_events is a really complicated endpoint in terms of return
@@ -55,87 +60,87 @@ class TestBeatmapsetEvents(TestCase):
         # and the osu! api isn't doing anything wrong by returning something
         # that doesn't match their documentation.
         for event_type in BeatmapsetEventType:
-            api_v2.beatmapset_events(types=[event_type])
+            api.beatmapset_events(types=[event_type])
 
 class TestRanking(TestCase):
     def test_deserialize(self):
-        api_v2.ranking("osu", RankingType.PERFORMANCE, country="US")
+        api.ranking("osu", RankingType.PERFORMANCE, country="US")
 
 class TestUserScores(TestCase):
     def test_deserialize(self):
-        api_v2.user_scores(12092800, "best")
+        api.user_scores(12092800, "best")
 
 class TestBeatmapUserScore(TestCase):
     def test_deserialize(self):
-        api_v2.beatmap_user_score(beatmap_id=221777, user_id=2757689, mode="osu")
+        api.beatmap_user_score(beatmap_id=221777, user_id=2757689, mode="osu")
 
 class TestBeatmapUserScores(TestCase):
     def test_deserialize(self):
-        api_v2.beatmap_user_scores(beatmap_id=221777, user_id=2757689, mode="osu")
+        api.beatmap_user_scores(beatmap_id=221777, user_id=2757689, mode="osu")
 
 class TestSearch(TestCase):
     def test_deserialize(self):
-        api_v2.search(query="peppy")
+        api.search(query="peppy")
 
 class TestComment(TestCase):
     def test_deserialize(self):
-        api_v2.comment(comment_id=1)
+        api.comment(comment_id=1)
 
 class TestSearchBeatmaps(TestCase):
     def test_deserialize(self):
-        api_v2.search_beatmapsets(query="the big black")
+        api.search_beatmapsets(query="the big black")
 
 class TestUser(TestCase):
     def test_deserialize(self):
-        api_v2.user(12092800)
+        api.user(12092800)
 
     def test_key(self):
         # make sure it automatically falls back to username if not specified
-        api_v2.user("tybug2")
-        api_v2.user("tybug2", key="username")
+        api.user("tybug2")
+        api.user("tybug2", key="username")
 
-        self.assertRaises(Exception, lambda: api_v2.user("tybug2", key="id"))
+        self.assertRaises(Exception, lambda: api.user("tybug2", key="id"))
 
 class TestWikiPage(TestCase):
     def test_deserialize(self):
-        api_v2.wiki_page("en", "Welcome")
+        api.wiki_page("en", "Welcome")
 
 class TestChangelogBuild(TestCase):
     def test_deserialize(self):
-        api_v2.changelog_build("stable40", "20210520.2")
+        api.changelog_build("stable40", "20210520.2")
 
 class TestChangelogListing(TestCase):
     def test_deserialize(self):
-        api_v2.changelog_listing()
+        api.changelog_listing()
 
 class TestChangelogLookup(TestCase):
     def test_deserialize(self):
-        api_v2.changelog_lookup("lazer")
+        api.changelog_lookup("lazer")
 
 class TestForumTopic(TestCase):
     def test_deserialize(self):
-        api_v2.forum_topic(141240)
+        api.forum_topic(141240)
 
 class TestBeatmapsetDiscussionVotes(TestCase):
     def test_deserialize(self):
-        api_v2.beatmapset_discussion_votes().votes[0].score
+        api.beatmapset_discussion_votes().votes[0].score
 
 class TestBeatmapsetDiscussions(TestCase):
     def test_deserialize(self):
-        api_v2.beatmapset_discussions()
+        api.beatmapset_discussions()
 
 class TestNewsPost(TestCase):
     def test_deserialize(self):
         # querying the same post by id or slug should give the same result.
-        post1 = api_v2.news_post(1025, key="id")
-        post2 = api_v2.news_post("2021-10-04-halloween-fanart-contest", key="slug")
+        post1 = api.news_post(1025, key="id")
+        post2 = api.news_post("2021-10-04-halloween-fanart-contest", key="slug")
 
         self.assertEqual(post1.id, post2.id)
         self.assertEqual(post1, post2)
 
 class TestSeasonalBackgrounds(TestCase):
     def test_deserialize(self):
-        api_v2.seasonal_backgrounds()
+        api.seasonal_backgrounds()
 
 
 
@@ -146,64 +151,64 @@ class TestSeasonalBackgrounds(TestCase):
 class TestCreateNewPM(TestCaseAuthorizationCode):
     def test_deserialize(self):
         # test_account https://osu.ppy.sh/users/14212521
-        api_v2_full.send_pm(14212521, "Unit test from ossapi "
+        api_full.send_pm(14212521, "Unit test from ossapi "
             "(https://github.com/circleguard/ossapi/), please ignore")
 
 class TestDownloadScore(TestCaseAuthorizationCode):
     def test_access_denied(self):
         # make sure client credentials api (`api`) can't access this endpoint
         self.assertRaises(AccessDeniedError,
-            lambda: api_v2.download_score(mode="osu", score_id=2797309065))
+            lambda: api.download_score(mode="osu", score_id=2797309065))
 
     def test_deserialize(self):
         # but the authorization code api (`api_v2_full`) can
-        api_v2_full.download_score(mode="osu", score_id=2797309065)
+        api_full.download_score(mode="osu", score_id=2797309065)
 
 class TestBeatmapAttributes(TestCase):
     def test_deserialize(self):
-        api_v2.beatmap_attributes(221777, ruleset="osu")
-        api_v2.beatmap_attributes(221777, mods=Mod.HDDT)
-        api_v2.beatmap_attributes(221777, mods="HR")
-        api_v2.beatmap_attributes(221777, ruleset_id=0)
+        api.beatmap_attributes(221777, ruleset="osu")
+        api.beatmap_attributes(221777, mods=Mod.HDDT)
+        api.beatmap_attributes(221777, mods="HR")
+        api.beatmap_attributes(221777, ruleset_id=0)
 
 class TestUsers(TestCase):
     def test_deserialize(self):
-        api_v2.users([12092800])
+        api.users([12092800])
 
 class TestBeatmaps(TestCase):
     def test_deserialize(self):
-        api_v2.beatmaps([221777])
+        api.beatmaps([221777])
 
 class TestScore(TestCase):
     def test_deserialize(self):
         # downloadable
-        api_v2.score(GameMode.OSU, 2243145877)
+        api.score(GameMode.OSU, 2243145877)
         # downloadable, my score
-        api_v2.score(GameMode.OSU, 3685255338)
+        api.score(GameMode.OSU, 3685255338)
         # not downloadable, my score
-        api_v2.score(GameMode.OSU, 3772000814)
+        api.score(GameMode.OSU, 3772000814)
 
         # other gamemodes
-        api_v2.score(GameMode.TAIKO, 176904666)
-        api_v2.score(GameMode.MANIA, 524674141)
-        api_v2.score(GameMode.CATCH, 211167989)
+        api.score(GameMode.TAIKO, 176904666)
+        api.score(GameMode.MANIA, 524674141)
+        api.score(GameMode.CATCH, 211167989)
 
 class TestMe(TestCaseAuthorizationCode):
     def test_insufficient_scope(self):
         # client credentials api can't request `Scope.IDENTIFY` and so can't
         # access /me
-        self.assertRaises(InsufficientScopeError, api_v2.get_me)
+        self.assertRaises(InsufficientScopeError, api.get_me)
 
     def test_deserialize(self):
         # but the authorization code api can
-        api_v2_full.get_me()
+        api_full.get_me()
 
 class TestFriends(TestCaseAuthorizationCode):
     def test_access_denied(self):
-        self.assertRaises(InsufficientScopeError, api_v2.friends)
+        self.assertRaises(InsufficientScopeError, api.friends)
 
     def test_deserialize(self):
-        api_v2_full.friends()
+        api_full.friends()
 
 # =====================
 # api_v2_dev test cases
@@ -211,7 +216,7 @@ class TestFriends(TestCaseAuthorizationCode):
 
 class TestForumCreateTopic(TestCaseDevServer):
     def test_create(self):
-        api_v2_dev.forum_create_topic("Integration test please ignore",
+        api_dev.forum_create_topic("Integration test please ignore",
             74, "Integration test please ignore")
 
     def test_create_with_poll(self):
@@ -222,14 +227,14 @@ class TestForumCreateTopic(TestCaseDevServer):
             "vote_change": True,
             "max_options": 1,
         }
-        api_v2_dev.forum_create_topic(body="Integration test with poll - please ignore" + str(datetime.now()),
+        api_dev.forum_create_topic(body="Integration test with poll - please ignore" + str(datetime.now()),
             forum_id=78,
             title="Integration test with poll - please ignore" + str(datetime.now()),
             with_poll=True, poll=poll)
 
 class TestForumReply(TestCaseDevServer):
     def test_reply(self):
-        api_v2_dev.forum_reply(156, "unit test from ossapi "
+        api_dev.forum_reply(156, "unit test from ossapi "
             "(https://github.com/circleguard/ossapi/), please ignore")
 
 # TODO first create a topic, then try and edit that one (this will fail if it's
@@ -237,9 +242,9 @@ class TestForumReply(TestCaseDevServer):
 
 class TestForumEditTopic(TestCaseDevServer):
     def test_edit(self):
-        api_v2_dev.forum_edit_topic(156, f"Title last updated at {datetime.now()}")
+        api_dev.forum_edit_topic(156, f"Title last updated at {datetime.now()}")
 
 class TestForumEditPost(TestCaseDevServer):
     def test_edit(self):
-        api_v2_dev.forum_edit_post(306,
+        api_dev.forum_edit_post(306,
             f"This comment was last edited at {datetime.now()}")
