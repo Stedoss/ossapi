6ac0f90ec651ca497a391d31e4837a86642920a8
Liam DeVoe <orionldevoe@gmail.com>
2023-08-12 17:57:56 -0400

    dont require homogeneous list in modt

--- ossapi/ossapiv2_async.py
+++ ossapi/ossapiv2_async.py
@@ -122,7 +122,7 @@ class Oauth2SessionAsync(OAuth2Session):
 # details).
 GameModeT = Union[GameMode, str]
 ScoreTypeT = Union[ScoreType, str]
-ModT = Union[Mod, str, int, Union[list[Mod], list[str], list[int]]]
+ModT = Union[Mod, str, int, list[Union[Mod, str, int]]]
 RankingFilterT = Union[RankingFilter, str]
 RankingTypeT = Union[RankingType, str]
 UserBeatmapTypeT = Union[UserBeatmapType, str]
